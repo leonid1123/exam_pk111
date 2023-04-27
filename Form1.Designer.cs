fcabfff7b8d47212da241d84109d6b0ebf0ea06d
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2023-04-27 14:04:00 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace exam_pk111
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.mb = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.proc = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.vid = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.bp = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(530, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Записать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(530, 382);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 70);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Считать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // mb
+            // 
+            this.mb.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.mb.Location = new System.Drawing.Point(48, 49);
+            this.mb.Name = "mb";
+            this.mb.Size = new System.Drawing.Size(434, 34);
+            this.mb.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Location = new System.Drawing.Point(48, 324);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(434, 207);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(45, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Материнская плата";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(45, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 29);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Процессор";
+            // 
+            // proc
+            // 
+            this.proc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.proc.Location = new System.Drawing.Point(48, 122);
+            this.proc.Name = "proc";
+            this.proc.Size = new System.Drawing.Size(434, 34);
+            this.proc.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(45, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 29);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Видеокарта";
+            // 
+            // vid
+            // 
+            this.vid.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.vid.Location = new System.Drawing.Point(48, 195);
+            this.vid.Name = "vid";
+            this.vid.Size = new System.Drawing.Size(434, 34);
+            this.vid.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(45, 234);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(174, 29);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Блок питания";
+            // 
+            // bp
+            // 
+            this.bp.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bp.Location = new System.Drawing.Point(48, 266);
+            this.bp.Name = "bp";
+            this.bp.Size = new System.Drawing.Size(434, 34);
+            this.bp.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 543);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.bp);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.vid);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.proc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.mb);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox mb;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox proc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox vid;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox bp;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using MySqlConnector;
+using System;
+using System.Windows.Forms;
+
+namespace exam_pk111
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection conn = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new MySqlConnection("Server=localhost;User ID=root;Password=root;Database=exam_pk111");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            string motherboard = mb.Text.Trim();
+            string processor = proc.Text.Trim();
+            string videocard = vid.Text.Trim();
+            string power = bp.Text.Trim();
+            string sqlIns = "INSERT INTO computers (motherboard,processor,videocard,power) VALUES (@mb,@pr,@vi,@po)";
+            MySqlCommand ins = new MySqlCommand(sqlIns, conn);
+            ins.Parameters.AddWithValue("@mb", motherboard);
+            ins.Parameters.AddWithValue("@pr", processor);
+            ins.Parameters.AddWithValue("@vi", videocard);
+            ins.Parameters.AddWithValue("@po", power);
+            ins.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            listBox1.Items.Clear();
+            string sqlSel = "SELECT motherboard,processor,videocard,power FROM computers";
+            MySqlCommand sel = new MySqlCommand(sqlSel, conn);
+            MySqlDataReader selRead = sel.ExecuteReader();
+            while (selRead.Read())
+            {
+                listBox1.Items.Add(selRead.GetString(0) + " " + selRead.GetString(1) + " " + selRead.GetString(2) + " " + selRead.GetString(3));
+            }
+            conn.Close();
+        }
+    }
+}
