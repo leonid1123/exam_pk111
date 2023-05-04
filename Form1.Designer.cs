db0e231dae025b8a94b9be1abb3448ff91b05296
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2023-05-04 17:03:58 +0300

    update,delete

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -42,15 +42,21 @@
             this.label5 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.удалениеЗаписейToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(530, 49);
+            this.button1.Location = new System.Drawing.Point(528, 167);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 70);
-            this.button1.TabIndex = 0;
+            this.button1.Size = new System.Drawing.Size(160, 58);
+            this.button1.TabIndex = 5;
             this.button1.Text = "Записать";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -58,10 +64,10 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(530, 461);
+            this.button2.Location = new System.Drawing.Point(528, 225);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(160, 70);
-            this.button2.TabIndex = 1;
+            this.button2.Size = new System.Drawing.Size(160, 58);
+            this.button2.TabIndex = 7;
             this.button2.Text = "Считать";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -69,27 +75,27 @@
             // mb
             // 
             this.mb.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.mb.Location = new System.Drawing.Point(48, 49);
+            this.mb.Location = new System.Drawing.Point(46, 109);
             this.mb.Name = "mb";
             this.mb.Size = new System.Drawing.Size(434, 34);
-            this.mb.TabIndex = 2;
+            this.mb.TabIndex = 1;
             // 
             // listBox1
             // 
             this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 29;
-            this.listBox1.Location = new System.Drawing.Point(48, 324);
+            this.listBox1.Location = new System.Drawing.Point(782, 109);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(434, 207);
-            this.listBox1.TabIndex = 3;
+            this.listBox1.Size = new System.Drawing.Size(434, 265);
+            this.listBox1.TabIndex = 10;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(45, 17);
+            this.label1.Location = new System.Drawing.Point(43, 77);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(241, 29);
             this.label1.TabIndex = 7;
@@ -99,7 +105,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(45, 90);
+            this.label2.Location = new System.Drawing.Point(43, 150);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(139, 29);
             this.label2.TabIndex = 9;
@@ -108,16 +114,16 @@
             // proc
             // 
             this.proc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.proc.Location = new System.Drawing.Point(48, 122);
+            this.proc.Location = new System.Drawing.Point(46, 182);
             this.proc.Name = "proc";
             this.proc.Size = new System.Drawing.Size(434, 34);
-            this.proc.TabIndex = 8;
+            this.proc.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(45, 163);
+            this.label3.Location = new System.Drawing.Point(43, 223);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(150, 29);
             this.label3.TabIndex = 11;
@@ -126,16 +132,16 @@
             // vid
             // 
             this.vid.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.vid.Location = new System.Drawing.Point(48, 195);
+            this.vid.Location = new System.Drawing.Point(46, 255);
             this.vid.Name = "vid";
             this.vid.Size = new System.Drawing.Size(434, 34);
-            this.vid.TabIndex = 10;
+            this.vid.TabIndex = 3;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(45, 234);
+            this.label4.Location = new System.Drawing.Point(43, 294);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(174, 29);
             this.label4.TabIndex = 13;
@@ -144,17 +150,17 @@
             // bp
             // 
             this.bp.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.bp.Location = new System.Drawing.Point(48, 266);
+            this.bp.Location = new System.Drawing.Point(46, 326);
             this.bp.Name = "bp";
             this.bp.Size = new System.Drawing.Size(434, 34);
-            this.bp.TabIndex = 12;
+            this.bp.TabIndex = 4;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label5.ForeColor = System.Drawing.Color.Red;
-            this.label5.Location = new System.Drawing.Point(488, 49);
+            this.label5.Location = new System.Drawing.Point(486, 109);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(0, 29);
             this.label5.TabIndex = 14;
@@ -163,10 +169,10 @@
             // 
             this.button3.Enabled = false;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button3.Location = new System.Drawing.Point(530, 248);
+            this.button3.Location = new System.Drawing.Point(528, 283);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(160, 70);
-            this.button3.TabIndex = 15;
+            this.button3.Size = new System.Drawing.Size(160, 58);
+            this.button3.TabIndex = 6;
             this.button3.Text = "Изменить";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
@@ -174,19 +180,71 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(530, 324);
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(528, 345);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(176, 20);
+            this.checkBox1.Size = new System.Drawing.Size(248, 29);
             this.checkBox1.TabIndex = 16;
             this.checkBox1.Text = "Разрешить изменения";
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.удалениеЗаписейToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1264, 28);
+            this.menuStrip1.TabIndex = 17;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // удалениеЗаписейToolStripMenuItem
+            // 
+            this.удалениеЗаписейToolStripMenuItem.Name = "удалениеЗаписейToolStripMenuItem";
+            this.удалениеЗаписейToolStripMenuItem.Size = new System.Drawing.Size(151, 24);
+            this.удалениеЗаписейToolStripMenuItem.Text = "Удаление записей";
+            this.удалениеЗаписейToolStripMenuItem.Click += new System.EventHandler(this.удалениеЗаписейToolStripMenuItem_Click);
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(118, 24);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(528, 109);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(160, 58);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Удалить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(41, 453);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(649, 145);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Тут описание моей супер-пупер-мега-гига программы\r\nАвторы всякие\r\nФичи\r\nПланы на " +
+    "будущее\r\nКончно пасхалка";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(739, 543);
+            this.ClientSize = new System.Drawing.Size(1264, 403);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label5);
@@ -201,9 +259,14 @@
             this.Controls.Add(this.mb);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Работа с БД";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -225,6 +288,11 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem удалениеЗаписейToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -9,6 +9,7 @@ namespace exam_pk111
     {
         //private MySqlConnection conn = null;
         List<Comp> mylist = new List<Comp>();
+        bool windowOpen = false;
         public Form1()
         {
             InitializeComponent();
@@ -32,21 +33,72 @@ namespace exam_pk111
             string processor = proc.Text.Trim();
             string videocard = vid.Text.Trim();
             string power = bp.Text.Trim();
-            string sqlIns = "INSERT INTO computers (motherboard,processor,videocard,power) VALUES (@mb,@pr,@vi,@po)";
-            MySqlCommand ins = new MySqlCommand(sqlIns, DBC.GetConn());
-            ins.Parameters.AddWithValue("@mb", motherboard);
-            ins.Parameters.AddWithValue("@pr", processor);
-            ins.Parameters.AddWithValue("@vi", videocard);
-            ins.Parameters.AddWithValue("@po", power);
-            ins.ExecuteNonQuery();
-            //conn.Close();
-            DBC.CloseConn();
+            if (motherboard.Length > 0 && processor.Length > 0 && videocard.Length > 0 && power.Length > 0)
+            {
+                string sqlIns = "INSERT INTO computers (motherboard,processor,videocard,power) VALUES (@mb,@pr,@vi,@po)";
+                MySqlCommand ins = new MySqlCommand(sqlIns, DBC.GetConn());
+                ins.Parameters.AddWithValue("@mb", motherboard);
+                ins.Parameters.AddWithValue("@pr", processor);
+                ins.Parameters.AddWithValue("@vi", videocard);
+                ins.Parameters.AddWithValue("@po", power);
+                ins.ExecuteNonQuery();
+                //conn.Close();
+                DBC.CloseConn();
+                GetDB();
+            }
+            else
+            {
+                MessageBox.Show("пустые поля нельзя!!!");
+            }
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            GetDB();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                button3.Enabled = true;
+            }
+            else
+            {
+                button3.Enabled = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            checkBox1.Checked = false;
+            int selectedID = mylist[listBox1.SelectedIndex].getId();
+            String upd = "UPDATE computers SET motherboard=@mb,processor=@pr,videocard=@vc,power=@pw WHERE id=@id";
+            MySqlCommand updCmd = new MySqlCommand(upd, DBC.GetConn());
+            updCmd.Parameters.AddWithValue("@mb", mb.Text.Trim());
+            updCmd.Parameters.AddWithValue("@pr", proc.Text.Trim());
+            updCmd.Parameters.AddWithValue("@vc", vid.Text.Trim());
+            updCmd.Parameters.AddWithValue("@pw", bp.Text.Trim());
+            updCmd.Parameters.AddWithValue("@id", selectedID);
+            updCmd.ExecuteNonQuery();
+            GetDB();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Console.WriteLine(listBox1.SelectedIndex);
+            //Console.WriteLine(mylist[listBox1.SelectedIndex].PrintInfo());
+            String[] allInfo = mylist[listBox1.SelectedIndex].PrintInfo().Split('|');
+            mb.Text = allInfo[0];
+            proc.Text = allInfo[1];
+            vid.Text = allInfo[2];
+            bp.Text = allInfo[3];
+        }
+        void GetDB()
+        {
             mylist.Clear();
-            //conn.Open();
             listBox1.Items.Clear();
             string sqlSel = "SELECT motherboard,processor,videocard,power,id FROM computers ORDER BY id ASC";
             MySqlCommand sel = new MySqlCommand(sqlSel, DBC.GetConn());
@@ -55,42 +107,45 @@ namespace exam_pk111
             {
                 Comp newComp = new Comp(selRead.GetInt32(4), selRead.GetString(0), selRead.GetString(1), selRead.GetString(2), selRead.GetString(3));
                 mylist.Add(newComp);
-                //listBox1.Items.Add(selRead.GetString(0) + " " + selRead.GetString(1) + " " + selRead.GetString(2) + " " + selRead.GetString(3));
             }
-            //conn.Close();
             DBC.CloseConn();
-            foreach (var item  in mylist)
+            foreach (var item in mylist)
             {
                 listBox1.Items.Add(item.PrintInfo());
             }
         }
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        private void удалениеЗаписейToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if(checkBox1.Checked)
+            button4.Visible = !button4.Visible;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
             {
-                button3.Enabled = true;
-            } else
-            {
-                button3.Enabled = false;
+
+                int selectedID = mylist[listBox1.SelectedIndex].getId();
+                String del = "DELETE FROM computers WHERE id=@id";
+                MySqlCommand delComm = new MySqlCommand(del, DBC.GetConn());
+                delComm.Parameters.AddWithValue("@id", selectedID);
+                delComm.ExecuteNonQuery();
+                GetDB();
             }
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            button3.Enabled=false;
-            checkBox1.Checked=false;
-        }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            Console.WriteLine(listBox1.SelectedIndex);
-            Console.WriteLine(mylist[listBox1.SelectedIndex].PrintInfo());       
-            String[] allInfo = mylist[listBox1.SelectedIndex].PrintInfo().Split('|');
-            mb.Text = allInfo[0];
-            proc.Text = allInfo[1];
-            vid.Text = allInfo[2];
-            bp.Text = allInfo[3];
+            //450-660
+            Form1 form = this;
+            windowOpen = !windowOpen;
+            if (windowOpen)
+            {
+                form.Height = 660;
+            } else
+            {
+                form.Height = 400;
+            }
         }
     }
 }
